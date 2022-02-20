1ee7dd596fb236058886095bcccb52477656bd74
stefanlester <97466386+stefanamoah@users.noreply.github.com>
2022-02-20 01:50:11 +0100

    feat : users to likes map

---
+++ usersmap.js
@@ -0,0 +1,16 @@
+const myUsers = [
+    { name: 'shark', likes: 'ocean' },
+    { name: 'turtle', likes: 'pond' },
+    { name: 'otter', likes: 'fish biscuits' }
+]
+
+const usersByLikes = myUsers.map(item => {
+    const container = {};
+
+    container[item.name] = item.likes;
+    container.age = item.name.length * 10;
+
+    return container;
+})
+
+console.log(usersByLikes);
