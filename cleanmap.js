b7ed2d52bfd06a68e27d0248dd20417a343404a0
stefanlester <97466386+stefanamoah@users.noreply.github.com>
2022-02-20 01:53:35 +0100

    feat : clean mapping method

---
+++ cleanmap.js
@@ -0,0 +1,10 @@
+// create a function to use
+const makeSweeter = sweetItem => sweetItem * 2;
+
+// we have an array
+const sweetArray = [2, 3, 4, 5, 35];
+
+// call the function we made. more readable
+const sweeterArray = sweetArray.map(makeSweeter);
+
+console.log(sweeterArray);
