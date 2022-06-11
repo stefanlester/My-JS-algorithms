3888419ef21b3c1f4f7b17064f3dd70cd93a7583
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-06-11 15:06:59 +0200

    refactor : radix sort

--- radixSort.js
+++ radixSort.js
@@ -21,3 +21,45 @@
 // Time Complexity (Worst)    - O(nk)
 // Space Complexity           - O(n + k)
 // n - length of array
+// k is the max number of digits, word size
+
+
+// returns the digit in num at the given place value
+function getDigit(num, i) {
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+  }
+  
+  // returns the number of digits in num
+  function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+  
+  // Given an array of numbers, returns the number of digits in the largest number in the list
+  function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+      maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+  }
+  
+  
+  function radixSort(nums){
+      let maxDigitCount = mostDigits(nums);
+      for(let k = 0; k < maxDigitCount; k++){
+  
+          let digitBuckets = Array.from({length: 10}, () => []);
+  
+          for(let i = 0; i < nums.length; i++){
+              let digit = getDigit(nums[i], k);
+              digitBuckets[digit].push(nums[i]);
+          }
+  
+          nums = [].concat(...digitBuckets);
+      }
+  
+      return nums;
+  }
+  
+  console.log(radixSort([23,345,5467,12,2345,9852])) // [12, 23, 345, 2345, 5467, 9852]
