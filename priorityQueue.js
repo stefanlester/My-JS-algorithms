c6a6fdaae62f9daa938ec2747017199cca7f82bc
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-07-24 22:17:03 +0200

    feat: prioroty queue

---
+++ priorityQueue.js
@@ -0,0 +1,11 @@
+// WHAT IS A PRIORITY QUEUE?
+
+// A data structure where each element has a priority.
+// Elements with higher priorities are served before elements with lower priorities.
+
+// OUR PRIORITY QUEUE
+
+// Write a Min Binary Heap - lower number means higher priority.
+// Each Node has a val and a priority.  Use the priority to build the heap.
+// Enqueue method accepts a value and priority, makes a new node, and puts it in the right spot based off of its priority.
+// Dequeue method removes root element, returns it, and rearranges heap using priority
